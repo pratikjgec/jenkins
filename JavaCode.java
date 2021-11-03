7baf0f47fd49ff63b747e70f408847ada5a973c9
Pratik Dutta <pratikduttaat@gmail.com>
2021-11-03 10:51:45 +0530

    java code added

---
+++ JavaCode.java
@@ -0,0 +1,12 @@
+class JavaCode
+{
+	public static void main(String args[])
+	{
+		System.out.println("This is java program----");
+		for(int i=0;i<5;i++)
+		{
+			System.out.println("value of i="+i);
+		}
+
+	}
+}
